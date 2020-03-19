83b09cd15bc0132b5d6960ae674829b0a9d59350
mpanska <47063770+mpanska@users.noreply.github.com>
2020-03-19 20:29:13 +0100

    Add files via upload

---
+++ lab1/content.py
@@ -0,0 +1,101 @@
+# --------------------------------------------------------------------------
+# ------------  Metody Systemowe i Decyzyjne w Informatyce  ----------------
+# --------------------------------------------------------------------------
+#  Zadanie 1: Regresja liniowa
+#  autorzy: A. Gonczarek, J. Kaczmar, S. Zareba
+#  2017
+# --------------------------------------------------------------------------
+
+import numpy as np
+from numpy.linalg import inv
+from utils import polynomial
+
+
+def mean_squared_error(x, y, w):
+    """
+    :param x: ciąg wejściowy Nx1
+    :param y: ciąg wyjsciowy Nx1
+    :param w: parametry modelu (M+1)x1
+    :return: błąd średniokwadratowy pomiędzy wyjściami y oraz wyjściami
+     uzyskanymi z wielowamiu o parametrach w dla wejść x
+    """
+    # N = polynomial(x,w).shape[0]  # to jest nasze N; polynomial - y z daszkiem
+    N = len(x)
+    sum = 0.0
+    for n in range(N):
+        sum += (y[n] - polynomial(x[n], w)) ** 2    # n - numer przykladu ze zbioru x
+    return sum / N
+
+
+def design_matrix(x_train, M):
+    """
+    :param x_train: ciąg treningowy Nx1
+    :param M: stopień wielomianu 0,1,2,...
+    :return: funkcja wylicza Design Matrix Nx(M+1) dla wielomianu rzędu M
+    """
+    N = len(x_train)
+    des_matrix = np.array[N][M + 1]
+    for n in range(N):
+        for m in range(M + 1):
+            des_matrix[n, m] = x_train[n] ** m
+    return des_matrix
+
+
+def least_squares(x_train, y_train, M):
+    """
+    :param x_train: ciąg treningowy wejśćia Nx1
+    :param y_train: ciąg treningowy wyjscia Nx1
+    :param M: rzad wielomianu
+    :return: funkcja zwraca krotkę (w,err), gdzie w są parametrami dopasowanego 
+    wielomianu, a err to błąd średniokwadratowy dopasowania
+    """
+    f = design_matrix(x_train, M)                              # ze wzoru (2) inv - macierz odwrotna
+    w = (inv(f.T.dot(f))).dot(f.T).dot(y_train)
+    err = mean_squared_error(x_train, y_train, w)
+    return w, err
+
+
+def regularized_least_squares(x_train, y_train, M, regularization_lambda):
+    """
+    :param x_train: ciąg treningowy wejśćia Nx1
+    :param y_train: ciąg treningowy wyjscia Nx1
+    :param M: rzad wielomianu
+    :param regularization_lambda: parametr regularyzacji
+    :return: funkcja zwraca krotkę (w,err), gdzie w są parametrami dopasowanego
+    wielomianu zgodnie z kryterium z regularyzacją l2, a err to błąd 
+    średniokwadratowy dopasowania
+    """
+    pass
+
+
+def model_selection(x_train, y_train, x_val, y_val, M_values):
+    """
+    :param x_train: ciąg treningowy wejśćia Nx1
+    :param y_train: ciąg treningowy wyjscia Nx1
+    :param x_val: ciąg walidacyjny wejśćia Nx1
+    :param y_val: ciąg walidacyjny wyjscia Nx1
+    :param M_values: tablica stopni wielomianu, które mają byc sprawdzone
+    :return: funkcja zwraca krotkę (w,train_err,val_err), gdzie w są parametrami
+    modelu, ktory najlepiej generalizuje dane, tj. daje najmniejszy błąd na 
+    ciągu walidacyjnym, train_err i val_err to błędy na sredniokwadratowe na 
+    ciągach treningowym i walidacyjnym
+    """
+    pass
+
+
+def regularized_model_selection(x_train, y_train, x_val, y_val, M, lambda_values):
+    """
+    :param x_train: ciąg treningowy wejśćia Nx1
+    :param y_train: ciąg treningowy wyjscia Nx1
+    :param x_val: ciąg walidacyjny wejśćia Nx1
+    :param y_val: ciąg walidacyjny wyjscia Nx1
+    :param M: stopień wielomianu
+    :param lambda_values: lista z wartościami różnych parametrów regularyzacji
+    :return: funkcja zwraca krotkę (w,train_err,val_err,regularization_lambda),
+    gdzie w są parametrami modelu, ktory najlepiej generalizuje dane, tj. daje
+    najmniejszy błąd na ciągu walidacyjnym. Wielomian dopasowany jest wg
+    kryterium z regularyzacją. train_err i val_err to błędy średniokwadratowe
+    na ciągach treningowym i walidacyjnym. regularization_lambda to najlepsza
+    wartość parametru regularyzacji
+    """
+    pass
